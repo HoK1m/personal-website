820c05517955d0202a1f2d564e7b7dda89ed1bd4
Howard Kim <howard.mkf@gmail.com>
2021-05-17 00:13:11 -0700

    first draft spaceship case study

--- src/pages/breeze.js
+++ src/pages/breeze.js
@@ -18,7 +18,6 @@ import wireframe2 from "../images/Landing_Page-2.png"
 import wireframe3 from "../images/Landing_Page-3.png"
 import wireframe4 from "../images/Landing_Page-4.png"
 import wireframe5 from "../images/Landing_Page-5.png"
-import wireframe6 from "../images/Landing_Page-3.png"
 import wireframe7 from "../images/settings1.png"
 import wireframe8 from "../images/settings2.png"
 import wireframe9 from "../images/settings3.png"
@@ -47,7 +46,7 @@ const Breeze = () => {
     const useCaseRef = useRef(null);
     const groundworkRef = useRef(null);
     const refineRef = useRef(null);
-    const adjustmentRef = useRef(null);
+    const improvisationRef = useRef(null);
     const reflectionRef = useRef(null);
 
     const sectionRefs = [
@@ -55,7 +54,7 @@ const Breeze = () => {
         { section: "Use Cases", ref: useCaseRef },
         { section: "Groundwork", ref: groundworkRef },
         { section: "Refining", ref: refineRef },
-        { section: "Adjustments", ref: adjustmentRef },
+        { section: "Improvisation", ref: improvisationRef },
         { section: "Reflection", ref: reflectionRef },
     ];
 
@@ -94,7 +93,7 @@ const Breeze = () => {
         const { height: headerHeight } = getDimensions(headerRef.current);
         const headerHeightOffset = -Math.abs(headerHeight);
         const y = ele.getBoundingClientRect().top + window.pageYOffset + headerHeightOffset;
-    
+
         window.scrollTo({
             top: y,
             behavior: "smooth",
@@ -139,9 +138,9 @@ const Breeze = () => {
                         </li>
                         <li>
                             <button
-                                className={`ss-navbutton ${visibleSection === "Adjustments" ? "ss-navSelected" : ""}`}
-                                onClick={() => { scrollTo(adjustmentRef.current); }}
-                            >Adjustments</button>
+                                className={`ss-navbutton ${visibleSection === "Improvisation" ? "ss-navSelected" : ""}`}
+                                onClick={() => { scrollTo(improvisationRef.current); }}
+                            >Improvisation</button>
                         </li>
                         <li>
                             <button
@@ -153,25 +152,72 @@ const Breeze = () => {
                 </div>
             </header>
 
-            <section className="ss-section" id="Background" ref={backgroundRef} style={{paddingTop: "1.75rem"}}>
+            <section className="ss-section" id="Background" ref={backgroundRef} style={{ paddingTop: "1.75rem" }}>
                 <h1>Breeze</h1>
                 <img className="ss-projImage" width="100%" height="auto" src={slide} alt="project content" />
                 <h3>Background</h3>
-                <p></p>
+                <p>
+                    Back when I first started at UC San Diego, what I found to be significantly inconvenient was the
+                    fact that there was a lack of online resources for UCSD campus organizations. All I found was a
+                    list of currently active clubs (that was registered with Associated Students) for that particular
+                    school year. This list would especially be sparse when it's the first few weeks of the academic
+                    year because there would be some clubs that weren’t ‘renewed’ for that academic year resulting in
+                    not showing up on the list. Even if particular clubs were on that list, the amount of information
+                    that I can access about that club was very limited and skimpy. There was a need to change that. A
+                    way for students to access primary contact information for these clubs/organizations as well as
+                    the club official website or page on a social media platform were all very much needed.
+                </p>
+                <h2 style={{ fontFamily: "Open Sans" }}>
+                    " Because of the lack of school resources online, students looking for a specific area of interest
+                    or finding information in clubs/organizations of their interests can be difficult. "
+                </h2>
             </section>
 
             <section className="ss-section" id="Use Cases" ref={useCaseRef}>
                 <h3>Design Use Cases / User Stories</h3>
+                <p>
+                    For this project, the foundation of all deliverables and implementations were based on the 
+                    descriptions outlined by the 'Use Cases'. Each 'Use Case' is a detailed description and the steps 
+                    involved with a users' interaction with the application on how it provides functionality. A 'Use Case' 
+                    is needed for each complete idea implemented by the application. Some examples of 'Use Case' includes 
+                    the following: Register, Login, Logout, View Page, Create Event, Edit Event, Add Events to Calendar.
+                    <br/><br/>
+                    In addition, 'User Stories' were used in tandem with 'Use Cases' to design and implement features of 
+                    the web platform. Each 'User Story' describes the type of user, what they want and why; basically 
+                    describing each 'Use Cases' from an end-user perspective. For Breeze, we divided the 'User Stories' 
+                    into three different cases: Baseline General User, Student User, and Campus Affiliate User. The 
+                    following list is few examples of what a typical 'User Story' would look like: 
+                    <ul>
+                        <li>As a general user, I want to log-in, so that I can be secure and save the preferences linked to my account.</li>
+                        <li>As a general user, I want to be able to message administrators, so that I can contact administrators with any inquiry or feedback that I have.</li>
+                        <li>As a general user, I want to be able to delete my account, so that my information will not be saved or viewable if I no longer want to use the site.</li>
+                        <br/>
+                        <li>As a student, I want to add my interests to the app and view them in a visually straightforward manner, so that the discovery feed can be personalized and tailored to show me the clubs which match my interests.</li>
+                        <li>As a student, I want to subscribe to affiliates so that I can keep up to date with the affiliates that I am interested in and receive their posts in my feed.</li>
+                        <li>As a student, I want to be able to search for organizations by name and by tag so that I can find organizations that cater to my interests.</li>
+                        <br/>
+                        <li>As a campus affiliate, I want to be able to modify the tags of my organization so that students of the app with matching interest will see my organization.</li>
+                        <li>As a campus affiliate, I want to post events to students subscribed to my org, and have those posts show up in their subscription feed, so that our subscribers can stay up-to-date with our events.</li>
+                        <li>As a campus affiliate, I want to maintain my affiliate page, so that students can see all my updated information compiled in one place.</li>
+                    </ul>
+                </p>
             </section>
 
             <section className="ss-section" id="Groundwork" ref={groundworkRef}>
                 <h3>Groundwork</h3>
-                <img className="ss-projImage" width="100%" height="auto" src={prototype1} alt="project content" />
-                <img className="ss-projImage" width="100%" height="auto" src={prototype2} alt="project content" />
+                <p>
+                    
+                </p>
+                <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-around" }}>
+                    <img className="ss-projImage" width="45%" height="auto" src={prototype1} alt="project content" />
+                    <img className="ss-projImage" width="45%" height="auto" src={prototype2} alt="project content" />
+                </div>
                 <img className="ss-projImage" width="100%" height="auto" src={prototype3} alt="project content" />
-                <img className="ss-projImage" width="100%" height="auto" src={prototype4} alt="project content" />
                 <img className="ss-projImage" width="100%" height="auto" src={prototype5} alt="project content" />
-                <img className="ss-projImage" width="100%" height="auto" src={prototype6} alt="project content" />
+                <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-around" }}>
+                    <img className="ss-projImage" width="40%" height="auto" src={prototype4} alt="project content" />
+                    <img className="ss-projImage" width="40%" height="auto" src={prototype6} alt="project content" />
+                </div>
             </section>
 
             <section className="ss-section" id="Refining" ref={refineRef}>
@@ -181,17 +227,20 @@ const Breeze = () => {
                 <img className="ss-projImage" width="100%" height="auto" src={wireframe3} alt="project content" />
                 <img className="ss-projImage" width="100%" height="auto" src={wireframe4} alt="project content" />
                 <img className="ss-projImage" width="100%" height="auto" src={wireframe5} alt="project content" />
-                <img className="ss-projImage" width="100%" height="auto" src={wireframe6} alt="project content" />
-                <img className="ss-projImage" width="100%" height="auto" src={wireframe7} alt="project content" />
                 <img className="ss-projImage" width="100%" height="auto" src={wireframe8} alt="project content" />
-                <img className="ss-projImage" width="100%" height="auto" src={wireframe9} alt="project content" />
+                <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-around" }}>
+                    <img className="ss-projImage" width="45%" height="auto" src={wireframe7} alt="project content" />
+                    <img className="ss-projImage" width="45%" height="auto" src={wireframe9} alt="project content" />
+                </div>
                 <img className="ss-projImage" width="100%" height="auto" src={wireframe10} alt="project content" />
-                <img className="ss-projImage" width="100%" height="auto" src={wireframe11} alt="project content" />
-                <img className="ss-projImage" width="100%" height="auto" src={wireframe12} alt="project content" />
+                <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-around" }}>
+                    <img className="ss-projImage" width="45%" height="auto" src={wireframe11} alt="project content" />
+                    <img className="ss-projImage" width="45%" height="auto" src={wireframe12} alt="project content" />
+                </div>
             </section>
 
-            <section className="ss-section" id="Adjustments" ref={adjustmentRef}>
-                <h3>Adjustments</h3>
+            <section className="ss-section" id="Improvisation" ref={improvisationRef}>
+                <h3>Improvisation</h3>
                 <img className="ss-projImage" width="100%" height="auto" src={search} alt="project content" />
             </section>
 
--- src/pages/spaceship.js
+++ src/pages/spaceship.js
@@ -88,7 +88,7 @@ const SpaceShip = () => {
         const { height: headerHeight } = getDimensions(headerRef.current);
         const headerHeightOffset = -Math.abs(headerHeight);
         const y = ele.getBoundingClientRect().top + window.pageYOffset + headerHeightOffset;
-    
+
         window.scrollTo({
             top: y,
             behavior: "smooth",
@@ -147,40 +147,125 @@ const SpaceShip = () => {
                 </div>
             </header>
 
-            <section className="ss-section" id="Background" ref={backgroundRef} style={{paddingTop: "1.75rem"}}>
+            <section className="ss-section" id="Background" ref={backgroundRef} style={{ paddingTop: "1.75rem" }}>
                 <h1>SpaceShip</h1>
                 <img className="ss-projImage" width="100%" height="auto" src={slide} alt="project content" />
                 <h3>Background</h3>
-                <p></p>
+                <p>
+                    One of my personal gripes about selling things from my hobby (mechanical keyboards) is that I often
+                    need to invoice my individual buyers so they can pay for the item before I ship it out to the buyer.
+                    However, the item collection (pick-up/delivery) is completely the buyer's responsibility; so in the
+                    case of when the item needs to be shipped out, I would need to invoice the buyer again for the
+                    shipping costs. <br />
+                    <br />
+                    Because the buyers have different preferences when it comes to the shipping carriers, I would have
+                    to individually get shipping quotes from those carriers. I needed a quicker, more efficient,
+                    one-stop solution so I can streamline my selling process within minutes. With just one-click, I can
+                    get shipping quotes from all domestic shipping carriers and include that shipping cost on the first
+                    invoice, eliminating the need for a second invoice.<br />
+                    <br />
+                    But maybe... maybe we can just make a more generalized solution? <br />
+                    Sending packages for the first time? Or maybe you just don’t send packages that often. The amount of
+                    domestic shipping carriers, their own list of shipping options, and their list of extra shipping
+                    services (like signature confirmation and insurance), it’s just way too overwhelming for an average
+                    person that doesn’t ship things often. What if there was a simple web tool that can simplify this
+                    process in just one-go? No need to check with each carrier one by one, just fill out your origin,
+                    destination, and package details followed by a single click to save ourselves the headache.
+                </p>
             </section>
 
             <section className="ss-section" id="Storyboarding" ref={storyboardRef}>
                 <h3>Storyboarding</h3>
-                <img className="ss-projImage" width="100%" height="auto" src={storyboard1} alt="project content" />
-                <img className="ss-projImage" width="100%" height="auto" src={storyboard2} alt="project content" />
-                <img className="ss-projImage" width="100%" height="auto" src={storyboard3} alt="project content" />
+                <p>
+                    Normally, we would start the design process by storyboarding, and this would address the problem
+                    statement using different potential solutions. However, my team and I were already considering quick
+                    and on-the-spot solutions during our 'needfinding' process. In our 'needfinding' process, we would
+                    each come up with personal/daily needs and throw them onto our board until they sticked. And when I
+                    say "they sticked", I mean that the 'needs' were capable of being more generalized to the masses, as
+                    well as considering whether our quick & on-the-spot solutions were feasible. <br />
+                    <br />
+                    For this particular focus, we realized there is no other way to go around the fact that we will always
+                    require users to enter a set of information and get results. So for our storyboards, we focused on
+                    potential target audience for our mobile webapp. We separated our target audience into three different
+                    groups: a typical consumer, small business owners, and corporations.<br />
+                    <br /><br />
+                </p>
+                <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-around" }}>
+                    <img className="ss-projImage" width="60%" height="auto" src={storyboard1} alt="project content" />
+                </div>
+                <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-around" }}>
+                    <img className="ss-projImage" width="40%" height="auto" src={storyboard2} alt="project content" />
+                    <img className="ss-projImage" width="40%" height="auto" src={storyboard3} alt="project content" />
+                </div>
             </section>
 
             <section className="ss-section" id="Prototyping" ref={prototypeRef}>
                 <h3>Prototyping</h3>
-                <img className="ss-projImage" width="100%" height="auto" src={prototype1} alt="project content" />
-                <img className="ss-projImage" width="100%" height="auto" src={prototype2} alt="project content" />
-                <img className="ss-projImage" width="100%" height="auto" src={prototype3} alt="project content" />
+                <p>
+                    For our next step of the design process, we went onto paper prototyping, which basically allows us 
+                    to start mocking up our mobile webapp design as paper and pen sketches. In this process, we were 
+                    stressed on the idea of 'buy-in' and 'ownership'; as a follow-up, we decided to come up 
+                    with mock-up designs individually, then come together again to start pitching each other our design 
+                    elements and ideas. This is contrary to the advice given by our mentor (to work on the prototypes 
+                    together) because I was concerned that our each individual vision of the final product would become 
+                    muddled/contaiminated by working together. <br/>
+                    <br/>
+                    Two of the paper prototypes came out relatively very similar to each other, so they were absorbed 
+                    into one. The final two paper prototypes that were submitted are shown below.
+                    <br/> <br/>
+                </p>
+                <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-around" }}>
+                    <img className="ss-projImage" width="40%" height="auto" src={prototype1} alt="project content" />
+                    <img className="ss-projImage" width="40%" height="auto" src={prototype2} alt="project content" />
+                </div>
+                <p>
+                    After few sessions of Heuristic Evaluation(HE) and gathering feedback, we came back to discuss what 
+                    particular design choices worked well and ran our bids on 'buy-in' and 'ownership'.  We also used this 
+                    chance to iterate on certain elements with QoL changes and improvements with the feedback from our HE 
+                    sessions.  The final paper prototype with combined design elements & improvements is shown below. <br/>
+                    <br/><br/>
+                </p>
+                <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-around" }}>
+                    <img className="ss-projImage" width="80%" height="auto" src={prototype3} alt="project content" />
+                </div>
             </section>
 
             <section className="ss-section" id="Wireframing" ref={wireframeRef}>
                 <h3>Wireframing</h3>
+                <p>
+                    Our wireframing process was different from others; this process was well-absorbed into our prototyping 
+                    process. Because one of my teammates (Melody Yu) was very comfortable with wireframing & Figma 
+                    mock-ups, we each took one paper prototypes to wireframe on Figma after we had our final two paper 
+                    prototypes to be submitted. These two sets of wireframes from the final two paper prototypes are shown 
+                    below.<br/>
+                    <br/> <br/>
+                </p>
                 <img className="ss-projImage" width="100%" height="auto" src={wireframe1} alt="project content" />
                 <img className="ss-projImage" width="100%" height="auto" src={wireframe2} alt="project content" />
+                <p>
+                    For our final paper prototype, Melody completed the wireframes while I completed the skeleton HTML/CSS 
+                    for the live version of our mobile webapp. Wireframe for the final paper prototype is shown below.<br/>
+                    <br/><br/>
+                </p>
                 <img className="ss-projImage" width="100%" height="auto" src={wireframe3} alt="project content" />
             </section>
 
             <section className="ss-section" id="Testing" ref={testRef}>
                 <h3>Testing</h3>
-            </section>
-
-            <section className="ss-section" id="Reflection" ref={reflectionRef}>
-                <h3>Reflection</h3>
+                <p>
+                    Our testing process was also different from others in the sense that we weren't necessarily testing 
+                    for the purpose of design but rather for the purpose of functionality. In terms of design user testing, 
+                    it was absorbed with the prototyping and wireframing process as Heuristic Evaluation Sessions. 
+                    However for the functionality testing, I had to make sure that the front-end is asking for the correct 
+                    necessary information for the API to fetch the shipping rate estimates. <br/>
+                    <br/>
+                    This process also includes making sure that the addresses inputted by the users are correctly being 
+                    validated (check if address exist), form validation (checking for valid numbers for package 
+                    specifications), and any other error handling that may be thrown by the API. During this phase, 
+                    Melody stepped up to fix up rest of the front-end and add some proper styling to the webapp. The final 
+                    product with proper styling is shown below. <br/>
+                    <br/><br/>
+                </p>
                 <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-around" }}>
                     <img className="ss-projImage" width="30%" height="auto" src={final1} alt="project content" />
                     <img className="ss-projImage" width="30%" height="auto" src={final2} alt="project content" />
@@ -189,6 +274,27 @@ const SpaceShip = () => {
                     <img className="ss-projImage" width="30%" height="auto" src={final3} alt="project content" />
                     <img className="ss-projImage" width="30%" height="auto" src={final4} alt="project content" />
                 </div>
+            </section>
+
+            <section className="ss-section" id="Reflection" ref={reflectionRef}>
+                <h3>Reflection</h3>
+                <p>
+                    Overall, I believe that this experience was a really good way for me to learn  
+                    one of the ways of going through a design process, as well as gaining experience in working 
+                    in teams and making difficult decisions. I learned that its possible for people to have extremely
+                    different work ethics, priorities/focus, as well as skills & their levels. Furthermore, I 
+                    learned how important it is to hold people accountable for their assigned tasks and the needed 
+                    soft skills in handling team dynamics. After all this, I still struggle in finding
+                    the best course of action on handling teammates that may refuse 'growth'; however, I realized that 
+                    it is still possible for those teammates to contribute as long as they work in their area of preference 
+                    or expertise. <br/>
+                    <br/>
+                    I want to personally thank Melody for stepping up with me to fill the lack of manpower and presevering
+                    throughout the rough situations and the project. I really appreciate the work that Melody put forward 
+                    in making sure the front-end gets well taken care of while I focused on the backend to ensure this 
+                    project is fully functional. In the future, I would like to continue on this project and build this 
+                    into production level application with more interesting and QoL features. 
+                </p>
             </section>
         </Layout >
     )
